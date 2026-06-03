c5f05d6adcde428499785d675790053669f6271b
valentimlopes81-collab <valentimlopes81@gmail.com>
2026-06-03 12:59:19 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1119,7 +1119,7 @@ PRODUCTS.men = ALL_PRODUCTS.filter(p =>
 
 // Mulher (Exclusivos + Unissexo)
 PRODUCTS.women = ALL_PRODUCTS.filter(p => 
-  [2, 3, 4, 5, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 31, 32, 33, 34, 35, 36, 38].includes(p.id)
+  [2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 31, 32, 33, 34, 35, 36, 38].includes(p.id)
 );
 
 // Garantir que os produtos têm a tag de género para o Quiz funcionar
